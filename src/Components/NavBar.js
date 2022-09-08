a403aa25f51e114c3c2c288ec4b23d38582ddf40
unknown <shreedhar281996@gmail.com>
2022-09-08 23:49:55 +0530

    first commit

---
+++ src/Components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='nav'>
+        <div className='navFont'>
+            <div id='smallDot'></div>
+            Solar Panel Quotes
+        </div>
+    </div>
+  )
+}
+export default NavBar
